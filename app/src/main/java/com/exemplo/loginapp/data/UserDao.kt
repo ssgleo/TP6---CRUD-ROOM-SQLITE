fdfba94790bbc4d4d8a603794fedebd794aa9e45
Leonardo Ribeiro <86031365+ssgleo@users.noreply.github.com>
2024-10-24 17:15:42 -0300

    Update UserDao.kt

--- app/src/main/java/com/exemplo/loginapp/data/UserDao.kt
+++ app/src/main/java/com/exemplo/loginapp/data/UserDao.kt
@@ -1,1 +1,18 @@
+package com.exemplo.loginapp.data
 
+import androidx.room.*
+
+@Dao
+interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUser(user: User)
+
+    @Query("SELECT * FROM user_table WHERE id = :userId LIMIT 1")
+    suspend fun getUserById(userId: Int): User?
+
+    @Update
+    suspend fun updateUser(user: User)
+
+    @Delete
+    suspend fun deleteUser(user: User)
+}
