8d3a235ece64a69139bf736c352d2eadd0a63449
Leonardo Ribeiro <86031365+ssgleo@users.noreply.github.com>
2024-10-24 17:21:52 -0300

    Update UserRepository.kt

--- app/src/main/java/com/exemplo/loginapp/data/UserRepository.kt
+++ app/src/main/java/com/exemplo/loginapp/data/UserRepository.kt
@@ -1,1 +1,19 @@
+package com.exemplo.loginapp.data
 
+class UserRepository(private val userDao: UserDao) {
+    suspend fun insertUser(user: User) {
+        userDao.insertUser(user)
+    }
+
+    suspend fun getUserById(id: Int): User? {
+        return userDao.getUserById(id)
+    }
+
+    suspend fun updateUser(user: User) {
+        userDao.updateUser(user)
+    }
+
+    suspend fun deleteUser(user: User) {
+        userDao.deleteUser(user)
+    }
+}
