6d6ef6b43f5b2dbe4a5cb0502c092a13fd1d9a33
Leonardo Ribeiro <86031365+ssgleo@users.noreply.github.com>
2024-10-24 17:09:51 -0300

    Update User.kt

--- app/src/main/java/com/exemplo/loginapp/data/User.kt
+++ app/src/main/java/com/exemplo/loginapp/data/User.kt
@@ -1,1 +1,12 @@
+package com.exemplo.loginapp.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+data class User(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val email: String,
+    val profilePicture: String
+)
