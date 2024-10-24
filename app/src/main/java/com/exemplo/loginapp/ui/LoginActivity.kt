650604cac08b983cd8b35fdb3e7ee65897696284
Leonardo Ribeiro <86031365+ssgleo@users.noreply.github.com>
2024-10-24 17:28:24 -0300

    Update LoginActivity.kt

--- app/src/main/java/com/exemplo/loginapp/ui/LoginActivity.kt
+++ app/src/main/java/com/exemplo/loginapp/ui/LoginActivity.kt
@@ -1,1 +1,20 @@
+package com.exemplo.loginapp.ui
 
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.exemplo.loginapp.R
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        btnLogin.setOnClickListener {
+            // Simular login
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
